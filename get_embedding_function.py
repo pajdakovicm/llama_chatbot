e5f63977a42ed42786e9b4ef3556dc1716ec90db
Marija.Pajdakovic <marijapajdakovic1@gmail.com>
2025-11-05 15:20:21 +0100

    delete comments

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -1,25 +1,8 @@
-# from langchain_community.embeddings.ollama import OllamaEmbeddings
-# from langchain_community.embeddings.bedrock import BedrockEmbeddings
-
-
-# def get_embedding_function():
-#     embeddings = BedrockEmbeddings(
-#         credentials_profile_name="default", region_name="us-east-1"
-#     )
-#     # embeddings = OllamaEmbeddings(model="nomic-embed-text")
-#     return embeddings
-
-# from langchain_community.embeddings.ollama import OllamaEmbeddings
 from langchain_ollama import OllamaEmbeddings
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
 
 def get_embedding_function():
-    # Option 1: Use Ollama (local, no API keys needed)
-    # embeddings = OllamaEmbeddings(model="llama3.2")
+
     embeddings = OllamaEmbeddings(model="nomic-embed-text")
     return embeddings
-
-    # Option 2: Use Hugging Face (local, no API keys)
-    # embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    # return embeddings
